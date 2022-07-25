e3dc9339a728ea587f2b6e4be5a5995091967367
Messi002 <rostandmessi2@gmail.com>
2022-07-25 16:22:54 +0100

    bitCoin

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -41,4 +41,8 @@ class Dropdown {
     }
     return DropdownItems;
   }
+
+
+
+  
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -24,7 +24,7 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(
           () {
             selectedCurrency = value.toString();
-            print(value);
+           
           },
         );
       },
