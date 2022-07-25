4fe67b274b406dda573548a4f287a52cb0e213e6
Messi002 <rostandmessi2@gmail.com>
2022-07-25 11:21:24 +0100

    bitCoin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bitcointicker/price_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,18 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.lightBlue),
         scaffoldBackgroundColor: Colors.white,
         primaryColor: Colors.lightBlue,
         textTheme: const TextTheme(bodyText2: TextStyle(
-          color: Colors.black,
+          color: Colors.white,
         ))
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('BitCoinTicker')),
-        body: const Center(
-          child: Text('Hello world'),
-        ),
-      ),
+      home: const PriceScreen(),
     );
   }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -11,24 +11,38 @@ class _PriceScreenState extends State<PriceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text(
-        '😎 Coin Ticker'
-      )),
+      appBar: AppBar(title: const Text('😎 Coin Ticker')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget> [
-           Padding(
-            padding: EdgeInsets.fromLTRB(18.0,18.0,18.0,0),
-          child: Card(
-            color: Colors.lightBlue,
-            elevation: 5.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlue,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
             ),
-              child: Padding(padding: EdgeInsets.symmetric(vertical: 15.0,horizontal: )),
           ),
-          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            color: Colors.lightBlue,
+            padding: const EdgeInsets.only(bottom: 30.0),
+            child: null,
+          )
         ],
       ),
     );
