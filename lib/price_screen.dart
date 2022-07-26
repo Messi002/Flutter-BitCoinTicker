3116acb743db2ff9f138393d99cea7f9b12c5600
Messi002 <rostandmessi2@gmail.com>
2022-07-26 14:25:28 +0100

    bitCoin

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -92,3 +92,19 @@ class _PriceScreenState extends State<PriceScreen> {
 
 
 
+
+
+
+oid main(List<String> args) {
+  List<int> ticket1 = [45, 2, 9, 18, 12, 33];
+  List<int> ticket2 = [41, 17, 26, 32, 7, 35];
+
+    checkNumbers(ticket1);
+  checkNumbers(ticket2);
+}
+
+void checkNumbers(List<int> myNumbers) {
+  int winNum = 0;
+  for (int element in myNumbers) {
+    for (int num in winningNumbers) {
+      if (element == num) {
